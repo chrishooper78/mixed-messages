ac4d00a95e5573cd46704af53b1f75c26ceaafec
Chris Hooper <hello@chrishooper.me>
2021-06-08 17:56:58 +0100

    Create the message

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,7 +1,22 @@
-console.log("Welcome to Mixed Messages.")
+console.log('Welcome to Mixed Messages.')
 
 //Store message components
 
 const msgPartAOptions = ['traffic light', 'sea', 'envelope']
 const msgPartBOptions = ['car', 'shore', 'stamp']
-const msgPartCOptions = ["Don't look now, I'm changing.", "Nothing! It just makes waves.", "Stick with me, and we'll go places."]
+const msgPartCOptions = [
+  "Don't look now, I'm changing",
+  'Nothing! It just makes waves',
+  "Stick with me, and we'll go places",
+]
+
+//Create the message
+
+const msgPartASelection =
+  msgPartAOptions[Math.floor(Math.random() * msgPartAOptions.length)]
+const msgPartBSelection =
+  msgPartBOptions[Math.floor(Math.random() * msgPartBOptions.length)]
+const msgPartCSelection =
+  msgPartCOptions[Math.floor(Math.random() * msgPartCOptions.length)]
+
+console.log('What did the ' + msgPartASelection + ' say to the ' + msgPartBSelection + '?' + ' "' + msgPartCSelection + '!"')
