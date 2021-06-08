7b400910fec9a293fdb6f88cff54c483012701ca
Chris Hooper <hello@chrishooper.me>
2021-06-08 17:51:06 +0100

    Store message components

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,7 @@
 console.log("Welcome to Mixed Messages.")
+
+//Store message components
+
+const msgPartAOptions = ['traffic light', 'sea', 'envelope']
+const msgPartBOptions = ['car', 'shore', 'stamp']
+const msgPartCOptions = ["Don't look now, I'm changing.", "Nothing! It just makes waves.", "Stick with me, and we'll go places."]
